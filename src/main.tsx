4570e354be1e3a482a32f20268437ed0605e9b21
samukelok <samkelokay2@gmail.com>
2025-08-02 21:29:42 +0200

    Setup queryClient For Easy Search And Filtering Of The API

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,32 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from 'react-query'
 import App from './App.tsx'
 import { AuthProvider } from './context/AuthContext'
 import { AvatarProvider } from './context/AvatarContext.tsx'
 import './index.css'
 
-createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        
-        <BrowserRouter basename='/client'>
-         {/* <BrowserRouter basename='/'> */}
-            <AuthProvider>
-                <AvatarProvider>
-                <App />
-                </AvatarProvider>
-            </AuthProvider>
-            
-        </BrowserRouter>
+// Create a QueryClient instance
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 1, 
+    },
+  },
+})
 
-    </StrictMode>,
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <BrowserRouter basename='/client'>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <AvatarProvider>
+            <App />
+          </AvatarProvider>
+        </AuthProvider>
+      </QueryClientProvider>
+    </BrowserRouter>
+  </StrictMode>,
 )
