ffa62bb2434db6df2cd0a0318c13efc02180f77d
samukelok <samkelokay2@gmail.com>
2025-08-14 16:11:54 +0200

    Split Builds To Allow For Easy Chunks

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,32 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/build-client/',
-  server: {
-    port: 3000,
-    strictPort: true,
-  },
-  build: {
-    outDir: path.resolve(__dirname, '../../volvcrm/public/build-client'),
-    emptyOutDir: true,
-    manifest: true,
-  },
+    plugins: [react()],
+    resolve: {
+        alias: {
+            jquery: path.resolve(__dirname, 'node_modules/jquery/dist/jquery.min.js'),
+        },
+    },
+    optimizeDeps: {
+        include: ['jquery'],
+    },
+    base: '/build-client/',
+    server: {
+        port: 3000,
+        strictPort: true,
+    },
+    build: {
+        outDir: path.resolve(__dirname, '../../volvcrm/public/build-client'),
+        emptyOutDir: true,
+        manifest: true,
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('jquery') || id.includes('summernote')) {
+                        return 'editor-vendor'
+                    }
+                },
+            },
+        },
+    },
 })
